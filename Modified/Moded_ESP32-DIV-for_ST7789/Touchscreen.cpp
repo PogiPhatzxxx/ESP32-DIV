5edcf196e03c4c84022ed7e4f423b4952b21b664
PogiPhatzxxx <dphatzsayson@gmail.com>
2025-06-25 19:53:12 +0800

    Update Touchscreen.cpp

--- Modified/Moded_ESP32-DIV-for_ST7789/Touchscreen.cpp
+++ Modified/Moded_ESP32-DIV-for_ST7789/Touchscreen.cpp
@@ -7,5 +7,5 @@ bool feature_active = false;
 void setupTouchscreen() {
     touchscreenSPI.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS); 
     ts.begin(touchscreenSPI);
-    ts.setRotation(0);
+    ts.setRotation(2);
 }
